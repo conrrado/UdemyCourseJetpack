81df153aca75bf96817a1980452548cadebbae45
Conrrado Camacho <con.webmaster@gmail.com>
2020-04-12 09:59:32 -0300

    Implementação de DiffUtil para otimizar listas

--- app/src/main/java/com/ccamacho/udemycoursejetpack/foundations/BaseRecyclerAdapter.kt
+++ app/src/main/java/com/ccamacho/udemycoursejetpack/foundations/BaseRecyclerAdapter.kt
@@ -1,16 +1,18 @@
 package com.ccamacho.udemycoursejetpack.foundations
 
 import android.view.View
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 
 abstract class BaseRecyclerAdapter<T>(
-    protected val masterList: MutableList<T> = mutableListOf()
+    private val masterList: MutableList<T> = mutableListOf()
 ): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     fun updateList(list: MutableList<T>) {
+        val result = DiffUtil.calculateDiff(DiffUtilCallbackImpl(masterList, list))
         masterList.clear()
         masterList.addAll(list)
-        notifyDataSetChanged()
+        result.dispatchUpdatesTo(this)
     }
 
     override fun getItemViewType(position: Int): Int =
@@ -42,5 +44,19 @@ abstract class BaseRecyclerAdapter<T>(
         const val FIRST_ITEM = 0
         const val TYPE_ADD_BUTTON = 0
         const val TYPE_INFO = 1
+    }
+
+    class DiffUtilCallbackImpl<T>(private val oldList: List<T>, private val newList: List<T>): DiffUtil.Callback() {
+
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+            oldList[oldItemPosition] == newList[newItemPosition]
+
+        override fun getOldListSize(): Int = oldList.size
+
+        override fun getNewListSize(): Int = newList.size
+
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+            oldList[oldItemPosition] == newList[newItemPosition]
+
     }
 }
